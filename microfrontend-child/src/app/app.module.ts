c5e6cff2ef6e76ca37d67ae9c49f54507641a35b
Vatsala Agrawal <vatsala.glory@gmail.com>
2020-01-24 11:38:02 +0100

    added component

--- microfrontend-child/src/app/app.module.ts
+++ microfrontend-child/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [AdminComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector){
+  }
+
+  ngDoBootstrap(){
+    const mycomponent = createCustomElement(AdminComponent, {injector: this.injector});
+    customElements.define('app-admin', mycomponent);
+  }
+}
